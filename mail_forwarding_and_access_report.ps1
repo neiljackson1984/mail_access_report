799b17b8393abffcd6c6d179f46dcb5fc84fa0ac
Neil Jackson <neil@rattnow.com>
2021-08-26 18:40:55 -0700

    converting Legacy Exchange x500 addresses (aka LegacyExchangeDN) into SMTP addresses, where possible.

--- mail_forwarding_and_access_report.ps1
+++ mail_forwarding_and_access_report.ps1
@@ -7,6 +7,61 @@ $licensedAzureAdUsers = $allAzureAdUsers | where {$_.AssignedLicenses.Length -gt
 
 
 "" | Out-File -FilePath $pathOfOutputReportFile
+
+
+
+function convertRedirectEntryToAddress($redirectEntry){
+    # redirectEntry is expected to be a string like the members
+    # of an InboxRule's RedirectTo property.
+    $pattern='^"([^"]*)"\s*\[([^:]*):(.*)\]$'
+    #when we apply $pattern to redirectEntry, the matching groups will pull 
+    # out the part between quotes, the protocol name, and the address, respectively.
+    
+    
+    #example $redirectEntry=='"John Doe" [SMTP:jdoe@acme.com]'
+    #the matching groups will be:
+    #$1: John Doe
+    #$2: SMTP
+    #$3: jdoe@acme.com    
+    
+    
+    #example $redirectEntry=='"John Doe" [EX:/o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=39e06be27d4a4e3e813d7ea40b95fa3f-jdoe]'
+
+    #the matching groups will be:
+    #$1: John Doe
+    #$2: EX
+    #$3: /o=ExchangeLabs/ou=Exchange Administrative Group (FYDIBOHF23SPDLT)/cn=Recipients/cn=39e06be27d4a4e3e813d7ea40b95fa3f-jdoe
+    
+    #in either case, we can attempt to "convert the address to SMTP by attempting to do (Get-Recipient $3).primarySMTPAddress
+    # if this throws a "couldn't find" error, then we return $3 as is.
+    $matches = $null
+    $result = $redirectEntry -match $pattern
+    
+    # if(!$result){
+        # #this is unexpected, and would constitute an error.
+        # # return "NOMATCH " + ([String] $redirectEntry)
+        # return $redirectEntry
+    # }
+       
+
+    if($result){
+        $resolvedSMTPAddress = (Get-Recipient -Identity $matches[3]).PrimarySmtpAddress  2>$null
+        if($resolvedSMTPAddress){
+            $resolvedSMTPAddress
+        } elseif($matches[3]) {
+            $matches[3]
+        } else {
+            $redirectEntry
+        }
+    } else {
+        $redirectEntry
+    }
+        
+
+}
+
+
+
 foreach($mailbox in (get-mailbox | Sort-Object -Property PrimarySmtpAddress)){
     $azureAdUserWhoOwnsThisMailbox = $allAzureAdUsers | where {$_.UserPrincipalName -eq $mailbox.PrimarySmtpAddress}
     
@@ -40,8 +95,11 @@ foreach($mailbox in (get-mailbox | Sort-Object -Property PrimarySmtpAddress)){
         ([system.Array] @($azureAdUserWhoOwnsThisMailbox))
     ) | Sort-Object -Property UserPrincipalName
     
+    $addressesToWhichThisMailboxIsBeingRedirected = (Get-InboxRule -Mailbox $mailbox.Identity) | foreach-object{ $_.RedirectTo; $_.ForwardTo; $_.ForwardAsAttachmentTo;  } |  Where-Object {$_} | foreach-object { convertRedirectEntryToAddress $_ } | sort
+    # we need the "|  Where-Object {$_}" in order to remove nulls from the pipeline, which happens when RedirectTo is, essentially, an empty list.
     
-    $addressesToWhichThisMailboxIsBeingRedirected = (Get-InboxRule -Mailbox $mailbox.Identity).RedirectTo | foreach-object {$_} | sort
+    #TODO: we should also look at the mailbox's ForwardingAddress and ForwardingSMTPAddress properties as possible sources of addresses to which this mailbox is being redirected.
+    
     $reportMessage = ""
     $reportMessage += "The mailbox " + $mailbox.PrimarySmtpAddress + " (which is a " + $(if($mailbox.IsShared){"shared"} else {"non-shared"}) +  " mailbox) " 
     
